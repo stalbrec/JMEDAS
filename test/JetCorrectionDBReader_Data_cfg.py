e90c915e6976394d5c111001a1e478ef8b9e14f0
Robin Aggleton <robin.aggleton@cern.ch>
2018-07-11 14:41:48 +0200

    Update GTs and collection names in JEC DBReaders

--- test/JetCorrectionDBReader_Data_cfg.py
+++ test/JetCorrectionDBReader_Data_cfg.py
@@ -3,7 +3,7 @@ process = cms.Process("jectxt")
 process.load('Configuration.StandardSequences.Services_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
 # define your favorite global tag
-process.GlobalTag.globaltag = '80X_dataRun2_Prompt_ICHEP16JEC_v0'
+process.GlobalTag.globaltag = '94X_dataRun2_ReReco_EOY17_v2'
 process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(1))
 process.source = cms.Source("EmptySource")
 process.readAK4PF    = cms.EDAnalyzer('JetCorrectorDBReader',  
@@ -11,16 +11,16 @@ process.readAK4PF    = cms.EDAnalyzer('JetCorrectorDBReader',
       payloadName    = cms.untracked.string('AK4PFchs'),
       # this is used ONLY for the name of the printed txt files. You can use any name that you like, 
       # but it is recommended to use the GT name that you retrieved the files from.
-      globalTag      = cms.untracked.string('80X_dataRun2_Prompt_ICHEP16JEC_v0'),  
+      globalTag      = cms.untracked.string('94X_dataRun2_ReReco_EOY17_v2'),  
       printScreen    = cms.untracked.bool(False),
       createTextFile = cms.untracked.bool(True)
 )
 process.readAK8PF    = cms.EDAnalyzer('JetCorrectorDBReader',  
       # below is the communication to the database 
-      payloadName    = cms.untracked.string('AK8PFchs'),
+      payloadName    = cms.untracked.string('AK8PFPuppi'),
       # this is used ONLY for the name of the printed txt files. You can use any name that you like, 
       # but it is recommended to use the GT name that you retrieved the files from.
-      globalTag      = cms.untracked.string('80X_dataRun2_Prompt_ICHEP16JEC_v0'),  
+      globalTag      = cms.untracked.string('94X_dataRun2_ReReco_EOY17_v2'),  
       printScreen    = cms.untracked.bool(False),
       createTextFile = cms.untracked.bool(True)
 )
