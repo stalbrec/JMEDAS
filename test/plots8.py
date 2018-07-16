3acd3c6f614a129b707ee830935f7969b9db9bdb
Robin Aggleton <robin.aggleton@cern.ch>
2018-07-16 14:39:20 +0200

    rename plotting scripts after removal of rho script

--- test/plots8.py
+++ test/plots8.py
@@ -2,32 +2,33 @@ from ROOT import *
 
 gROOT.Macro("rootlogon.C")
 
-f1 = TFile("ttjets_short.root")
-f2 = TFile("rsgluon_ttbar_3TeV.root")
+f1 = TFile("ttbar.root")
+f2 = TFile("grav_ww_3000.root")
 
-h_rhoRatioAK8_1   = f1.Get("h_rhoRatioAK8")
-h_rhoRatioAK8_2   = f2.Get("h_rhoRatioAK8")
+h_tau21AK8_1 = f1.Get("h_tau21AK8")
+h_tau21AK8_2 = f2.Get("h_tau21AK8")
 
-h_rhoRatioAK8_1    .SetLineColor(1)
-h_rhoRatioAK8_2    .SetLineColor(2)
+h_tau21AK8_1.SetLineColor(1)
+h_tau21AK8_2.SetLineColor(2)
 
-leg = TLegend(0.3, 0.5, 0.5, 0.8)
+leg = TLegend(0.15, 0.5, 0.35, 0.8)
 leg.SetFillColor(0)
 leg.SetBorderSize(0)
-leg.AddEntry( h_rhoRatioAK8_1, "t#bar{t}", 'l')
-leg.AddEntry( h_rhoRatioAK8_2, "RS KK Gluon", 'l')
+leg.AddEntry( h_tau21AK8_1, "t#bar{t}", 'l')
+# leg.AddEntry( h_tau21AK8_2, "Z'#rightarrow t#bar{t}", 'l')
+leg.AddEntry( h_tau21AK8_2, "G#rightarrow WW", 'l')
 
-h_rhoRatioAK8_1.Sumw2()
-h_rhoRatioAK8_1.Scale( 1.0 / h_rhoRatioAK8_1.Integral() )
-h_rhoRatioAK8_2.Sumw2()
-h_rhoRatioAK8_2.Scale( 1.0 / h_rhoRatioAK8_2.Integral() )
+h_tau21AK8_1.Sumw2()
+h_tau21AK8_1.Scale( 1.0 / h_tau21AK8_1.Integral() )
+h_tau21AK8_2.Sumw2()
+h_tau21AK8_2.Scale( 1.0 / h_tau21AK8_2.Integral() )
 
 c = TCanvas('c', 'c') 
-h_rhoRatioAK8_1    .Draw("hist")
-h_rhoRatioAK8_2    .Draw("same hist")
+h_tau21AK8_1.Draw("hist")
+h_tau21AK8_2.Draw("same hist")
 
 
 leg.Draw()
 
-c.Print('plots8.png', 'png')
-c.Print('plots8.pdf', 'pdf')
+c.Print('plots9.png', 'png')
+c.Print('plots9.pdf', 'pdf')
--- test/plots9.py
+++ test/plots9.py
@@ -3,32 +3,31 @@ from ROOT import *
 gROOT.Macro("rootlogon.C")
 
 f1 = TFile("ttbar.root")
-f2 = TFile("grav_ww_3000.root")
+f2 = TFile("zprime_ttbar_3000.root")
 
-h_tau21AK8_1 = f1.Get("h_tau21AK8")
-h_tau21AK8_2 = f2.Get("h_tau21AK8")
+h_tau32AK8_1 = f1.Get("h_tau32AK8")
+h_tau32AK8_2 = f2.Get("h_tau32AK8")
 
-h_tau21AK8_1.SetLineColor(1)
-h_tau21AK8_2.SetLineColor(2)
+h_tau32AK8_1.SetLineColor(1)
+h_tau32AK8_2.SetLineColor(2)
 
-leg = TLegend(0.15, 0.5, 0.35, 0.8)
+leg = TLegend(0.3, 0.5, 0.5, 0.8)
 leg.SetFillColor(0)
 leg.SetBorderSize(0)
-leg.AddEntry( h_tau21AK8_1, "t#bar{t}", 'l')
-# leg.AddEntry( h_tau21AK8_2, "Z'#rightarrow t#bar{t}", 'l')
-leg.AddEntry( h_tau21AK8_2, "G#rightarrow WW", 'l')
+leg.AddEntry( h_tau32AK8_1, "t#bar{t}", 'l')
+leg.AddEntry( h_tau32AK8_2, "Z'#rightarrow t#bar{t}", 'l')
 
-h_tau21AK8_1.Sumw2()
-h_tau21AK8_1.Scale( 1.0 / h_tau21AK8_1.Integral() )
-h_tau21AK8_2.Sumw2()
-h_tau21AK8_2.Scale( 1.0 / h_tau21AK8_2.Integral() )
+h_tau32AK8_1.Sumw2()
+h_tau32AK8_1.Scale( 1.0 / h_tau32AK8_1.Integral() )
+h_tau32AK8_2.Sumw2()
+h_tau32AK8_2.Scale( 1.0 / h_tau32AK8_2.Integral() )
 
 c = TCanvas('c', 'c') 
-h_tau21AK8_1.Draw("hist")
-h_tau21AK8_2.Draw("same hist")
+h_tau32AK8_1.Draw("hist")
+h_tau32AK8_2.Draw("same hist")
 
 
 leg.Draw()
 
-c.Print('plots9.png', 'png')
-c.Print('plots9.pdf', 'pdf')
+c.Print('plots10.png', 'png')
+c.Print('plots10.pdf', 'pdf')
