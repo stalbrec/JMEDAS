9f635a4ab874689afbebff33b17f7f69935c082b
Robin Aggleton <robin.aggleton@cern.ch>
2018-07-16 16:05:55 +0200

    Use QCD for tau21 plots, rebin

--- test/plots8.py
+++ test/plots8.py
@@ -2,7 +2,7 @@ from ROOT import *
 
 gROOT.Macro("rootlogon.C")
 
-f1 = TFile("ttbar.root")
+f1 = TFile("qcd_1400.root")
 f2 = TFile("grav_ww_3000.root")
 
 h_tau21AK8_1 = f1.Get("h_tau21AK8")
@@ -11,21 +11,23 @@ h_tau21AK8_2 = f2.Get("h_tau21AK8")
 h_tau21AK8_1.SetLineColor(1)
 h_tau21AK8_2.SetLineColor(2)
 
-leg = TLegend(0.15, 0.5, 0.35, 0.8)
+leg = TLegend(0.15, 0.65, 0.35, 0.85)
 leg.SetFillColor(0)
 leg.SetBorderSize(0)
-leg.AddEntry( h_tau21AK8_1, "t#bar{t}", 'l')
-# leg.AddEntry( h_tau21AK8_2, "Z'#rightarrow t#bar{t}", 'l')
-leg.AddEntry( h_tau21AK8_2, "G#rightarrow WW", 'l')
+leg.AddEntry( h_tau21AK8_1, "QCD", 'l')
+leg.AddEntry( h_tau21AK8_2, "G* #rightarrow WW", 'l')
 
 h_tau21AK8_1.Sumw2()
+h_tau21AK8_1.Rebin(4)
 h_tau21AK8_1.Scale( 1.0 / h_tau21AK8_1.Integral() )
 h_tau21AK8_2.Sumw2()
+h_tau21AK8_2.Rebin(4)
 h_tau21AK8_2.Scale( 1.0 / h_tau21AK8_2.Integral() )
 
 c = TCanvas('c', 'c') 
-h_tau21AK8_1.Draw("hist")
-h_tau21AK8_2.Draw("same hist")
+h_tau21AK8_2.Draw("hist")
+h_tau21AK8_1.Draw("same hist")
+h_tau21AK8_2.SetMaximum( h_tau21AK8_2.GetMaximum()*1.6 )
 
 
 leg.Draw()
